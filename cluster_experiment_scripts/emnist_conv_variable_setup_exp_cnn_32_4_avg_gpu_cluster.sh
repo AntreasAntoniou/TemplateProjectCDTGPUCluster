337426e12d8a09156405b7804c050806a12eda74
Antreas Antoniou <antrikohs@gmail.com>
2019-01-14 16:17:28 +0000

    Update emnist_conv_variable_setup_exp_cnn_32_4_avg_gpu_cluster.sh

--- cluster_experiment_scripts/emnist_conv_variable_setup_exp_cnn_32_4_avg_gpu_cluster.sh
+++ cluster_experiment_scripts/emnist_conv_variable_setup_exp_cnn_32_4_avg_gpu_cluster.sh
@@ -2,7 +2,7 @@
 #SBATCH -N 1	  # nodes requested
 #SBATCH -n 1	  # tasks requested
 #SBATCH --gres=gpu:1
-#SBATCH --mem=16000  # memory in Mb
+#SBATCH --mem=14000  # memory in Mb
 #SBATCH --time=0-05:00:00
 #SBATCH --exclude=charles[11-12]
 
